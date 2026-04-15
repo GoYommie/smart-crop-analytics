5b1d5e64acef5045d56c53a9ae8d47740296ffb2
GoYommie <isaiahadeyomi@gmail.com>
2026-04-15 15:34:43 -0600

    Implemented HTML template rendering for homepage using Django views

--- crop_project/crops/views.py
+++ crop_project/crops/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to Smart Crop Analytics System 🌾")
+    return render(request, 'crops/home.html')
