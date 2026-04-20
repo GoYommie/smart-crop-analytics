500f837b0977097150d6c76aafac8bf111320748
GoYommie <isaiahadeyomi@gmail.com>
2026-04-19 21:44:14 -0600

    Finalize Smart Crop Analytics project

--- crop_project/crop_project/settings.py
+++ crop_project/crop_project/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# paths inside the project should look like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production, secret!
 SECRET_KEY = 'django-insecure-mtv70r5vvx&65z%w4eszb$7v=3=sg0ikz!@+yzpc2=-$0*$soo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
